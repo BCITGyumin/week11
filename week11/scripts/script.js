edb5c00eee79a11df4db9ebd5ae1ebb334d0ca5c
Gyumin Kim <gyuminkim2002@gmail.com>
2023-03-20 14:23:22 -0700

    arrow function

--- week11/scripts/script.js
+++ week11/scripts/script.js
@@ -6,4 +6,9 @@ let greet = function(name) {        // function expression
     console.log(`Hello ${name}!`);
 }
 
+let greet = (name) => {             //arrow function
+    console.log(`Hello ${name}!`);
+}
+
 greet("Chris");
+
